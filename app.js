782b78d6d1648b21cc5a37917fef273861ae879c
Priyanshu Tiwari <you@example.com>
2022-01-20 21:45:02 +0530

    app working fine

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-const alert = require("alert");
 var axios = require("axios");
 app.set("view engine", "ejs");
 
 app.post("/", function (req, res) {
   var code = `` + req.body.code + ``;
   var lang = req.body.language;
-  var inputs = `` + req.body.inputs + ``;
+  var inputs = req.body.inputs;
+  // console.log(code, lang, inputs)
   var langArr = [false, false, false, false];
   var language = "";
   if (lang === "C") {
@@ -21,7 +21,7 @@ app.post("/", function (req, res) {
   } else if (lang === "Java") {
     language = "java";
     langArr[2] = true;
-  } else if(lang==="Python") {
+  } else if (lang === "Python") {
     language = "py";
     langArr[3] = true;
   }
@@ -44,13 +44,14 @@ app.post("/", function (req, res) {
 
   axios(config)
     .then(function (response) {
-       
+      let output = response.data.output;
+      // console.log(output);
       res.render("index", {
         code: code,
         language: lang,
         inputs: inputs,
-        output: response.data.output,
         langArr: langArr,
+        output: output,
       });
     })
     .catch(function (error) {
@@ -63,7 +64,7 @@ app.get("/", function (req, res) {
     language: "",
     inputs: "",
     output: "",
-    langArr: [false, false, false, false]
+    langArr: [false, false, false, false],
   });
 });
 
